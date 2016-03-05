095501e8b9da97df6fbbd9c51d045552aee21a96
Jonathan <jonfpoole@gmail.com>
2016-03-05 13:17:24 +0000

    Added function call type checking to make sure that any calls to a function match the usage of the parameters inside the function

--- src/typeCheck.ml
+++ src/typeCheck.ml
@@ -27,18 +27,26 @@ type t =
   | Tint
   | Tfloat
   | Tbool
-  | Tfunction of t
+  | Tfunction of ((id*t) list) * t
   | Tunknown
   | Tarray of int (* The number of dimensions that the array has *)
 
 let rec show_t t =
+  let rec show_param_ts (params:(id*t) list) (show_string:string) =
+    match params with
+    | [] -> show_string
+    | (param_id, param_t)::params ->
+        let show_string = show_string ^ "(" ^ (show_id param_id) ^ ":" ^ (show_t param_t) ^ ") -> " in
+        show_param_ts params show_string
+  in
   match t with
   | Tint -> "int"
   | Tfloat -> "float"
   | Tbool -> "bool"
-  | Tfunction t -> "function " ^ show_t t
+  | Tfunction (params, t) -> "function of type " ^ (show_param_ts (List.rev params) "") ^ (show_t t)
   | Tunknown -> "unknown"
   | Tarray n -> "array " ^ string_of_int n
+      
 
 (* Map identifiers to their types *)
 type env_t = t Idmap.t
@@ -62,6 +70,14 @@ let op_result_type op : t =
   | (T.Plus | T.Minus | T.Times | T.Div | T.Lshift | T.BitOr | T.BitAnd) -> Tint
   | (T.Fplus | T.Fminus | T.Ftimes | T.Fdiv) -> Tfloat
   | (T.And | T.Or | T.Lt | T.Gt | T.Eq) -> Tbool
+
+let pp_env env =
+  let print_key k v = 
+    Printf.printf "key: %s, value %s\n" (show_id k) (show_t v);
+    ()
+  in
+  Idmap.iter print_key env;
+  Printf.printf "\n"
   
 (* Compute the type of an expression, or raise BadInput if there is a type
    error *)
@@ -108,8 +124,30 @@ let rec type_exp (ln : int option) (env : env_t) (e : exp) : t*env_t =
   | Num n -> (Tint, env)
   | Float n ->(Tfloat, env)
   | Bool b -> (Tbool, env)
-  | FunctionCall (id, parameters) ->
-     type_error ln "function call not implemented"
+  | FunctionCall (id, call_params) ->
+    let rec type_check_call_params call_params fun_params =
+      match (call_params, fun_params) with
+      | ([], _::_) | (_::_, []) -> type_error ln "Function parameter count doesn't match function call"
+      | (call_param::call_params, (fun_param_id, fun_param_t)::fun_params) ->
+        let (call_param_t, _) = type_exp ln env call_param in
+        if call_param_t = fun_param_t then
+          type_check_call_params call_params fun_params
+        else
+          type_error ln ("Error in call to function " ^ (show_id id) 
+            ^ " parameter " ^ (show_id fun_param_id) 
+            ^ " is of type " ^ (show_t fun_param_t) 
+            ^ " but expression is of type " ^ (show_t call_param_t))
+      | ([], []) -> ()
+    in
+    (try 
+      (match Idmap.find id env with
+        Tfunction (fun_param_list, return_type) -> 
+          type_check_call_params call_params fun_param_list;
+          (return_type, env))
+    with
+    | _ -> type_error ln ("Function " ^ (show_id id) ^ " not defined. No reccursion allowed"))
+    
+    
   | Op (e1, op, e2) ->
     let (exp_t, env) = infer_ident_type ln e1 op env in
     infer_ident_type ln e2 op env
@@ -138,20 +176,14 @@ let type_lhs_ident (env :env_t) (x : id) (t : t) (ln : int option) : env_t =
     match t_found with
     | Tunknown -> 
        let new_env = Idmap.add x t env in
-       (match x with
-	| Source k -> Printf.printf "type_lhs_ident %s: %s\n" k (show_t t)
-	| Temp (k, _) -> Printf.printf "type_lhs_ident %s: %s\n" k (show_t t));
        new_env
     | _ when t_found = t -> env
     | _ -> type_error ln ("Bad type for " ^ show_id x)
   with Not_found ->
     let new_env = Idmap.add x t env in
-    (match x with
-     | Source k -> Printf.printf "type_lhs_ident %s: %s\n" k (show_t t)
-     | Temp (k, _) -> Printf.printf "type_lhs_ident %s: %s\n" k (show_t t));
     new_env
 
-let join_environments outer_env inner_env : env_t =
+let join_environments ln outer_env inner_env : env_t =
   let contains k a b = 
     match (a, b) with
     | (Some Tunknown, Some inner) -> Some inner
@@ -159,18 +191,10 @@ let join_environments outer_env inner_env : env_t =
     | (Some outer, Some inner) when inner = outer ->
        Some outer
     | (Some outer, Some inner) ->
-       type_error (Some 0) ("Environment mismatch! Outer: " ^ (show_t outer) ^ " Inner: " ^ (show_t inner))
-    | _ -> type_error (Some 0) "error"
+       type_error ln ("Found type " ^ (show_t inner) ^ " when expecting type: " ^ (show_t outer))
+    | _ -> type_error ln "Compiler error: idents missing in inner enironment that are in outer environment\n"
   in
   Idmap.merge contains outer_env inner_env
-
-let pp_env env =
-  let print_key k v = 
-    Printf.printf "key: %s, value %s\n" (show_id k) (show_t v);
-    ()
-  in
-  Idmap.iter print_key env;
-  Printf.printf "\n"
 
 (* Type check a list of statements. Raise BadInput if there is an error.  Check
    a list so that earlier assignment statements can extend the environment for
@@ -188,13 +212,8 @@ let rec type_stmts (ln : int option) (env :env_t) (stmts : stmt list) (fun_id : 
       type_error ln "Output with non-integer type"
   | Assign (x, [], e) :: stmts' ->
     let (t, env) = type_exp ln env e in
-    let env' = match e with
-      (* for e to be assigned to t, e and t must be of the same type *)
-      | Ident (e_id, _) -> type_lhs_ident env e_id t ln
-      | _ -> env
-    in
-    let env' = type_lhs_ident env' x t ln in
-    type_stmts ln env' stmts' fun_id
+    let env = type_lhs_ident env x t ln in
+    type_stmts ln env stmts' fun_id
   | Assign (x, es, e) :: stmts' ->
     (* Assignments to arrays require the lhs to be checkable as an expression.
        In particular, the identifier must already be bound to an array type of
@@ -211,26 +230,39 @@ let rec type_stmts (ln : int option) (env :env_t) (stmts : stmt list) (fun_id : 
        match params with
        | [] -> env
        | param::params -> 
-	  let env = Idmap.add param Tunknown env in
-	  add_params params env
+         let env = Idmap.add param Tunknown env in
+	       add_params params env
      in
+     let rec get_param_types (param_ids:id list) (type_list:(id*t) list) (env: env_t): (id*t) list =
+       match param_ids with
+       | [] -> type_list
+       | param_id::param_ids -> 
+         let param_type:t = Idmap.find param_id env in
+         let id_type:id*t = (param_id, param_type) in
+         get_param_types param_ids (type_list@[id_type]) env
+     in  
      let env' = add_params parameters env in
      let env' = type_stmts ln env' [statements] (Some id) in
-     type_stmts ln env' stmts' fun_id
+     let env = join_environments ln env env' in
+     let Tfunction (_, fun_ret_type) = Idmap.find id env' in
+     let param_types = get_param_types parameters [] env' in
+     let env = Idmap.add id (Tfunction (param_types, fun_ret_type)) env in
+     type_stmts ln env stmts' fun_id
   | FunctionReturn exp :: stmts' ->
      (match fun_id with
       | Some fun_id -> 
-	 let (exp_type, env) = type_exp ln env exp in
-	 let env = type_lhs_ident env fun_id (Tfunction exp_type) ln in
-	 type_stmts ln env stmts' (Some fun_id)
+	      let (exp_type, env) = type_exp ln env exp in
+        pp_env env;
+	      let env = type_lhs_ident env fun_id (Tfunction ([], exp_type)) ln in
+	      type_stmts ln env stmts' (Some fun_id)
       | None -> type_error ln "return statement not in a function")     
   | DoWhile (s1, e, s2) :: stmts ->
     let env_s1 = type_stmts ln env [s1] fun_id in
     let (e_t, env) = type_exp ln env e in 
     if e_t = Tbool then
       (let env_s2 = type_stmts ln env [s2] fun_id in
-       let env = join_environments env env_s1 in
-       let env = join_environments env env_s2 in
+       let env = join_environments ln env env_s1 in
+       let env = join_environments ln env env_s2 in
        type_stmts ln env stmts fun_id)
     else
       type_error ln "While test of non-bool type"
@@ -238,9 +270,9 @@ let rec type_stmts (ln : int option) (env :env_t) (stmts : stmt list) (fun_id : 
     let (e_t, env) = type_exp ln env e in
     if e_t = Tbool then
       (let if_env = type_stmts ln env [s1] fun_id in
+       let env = join_environments ln env if_env in
        let else_env = type_stmts ln env [s2] fun_id in
-       let env = join_environments env if_env in
-       let env = join_environments env else_env in
+       let env = join_environments ln env else_env in
        type_stmts ln env stmts fun_id)
     else
       type_error ln "If test of non-bool type"
